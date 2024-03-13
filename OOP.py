f822f228408a4a9612e70f83dc9e51b81238a136
heera2507 <163078247+heera2507@users.noreply.github.com>
2024-03-13 13:33:13 +1100

    Create OOP.py

---
+++ OOP.py
@@ -0,0 +1,21 @@
+class Student;
+
+lowered_loan = 0.245
+num_of_students = 0 
+
+def __info__(self, first, last, loan):
+  self.first = first
+  self.last = last
+  self.loan = loan
+  self.email = first + '.' + last + '@edu.com'
+
+Student.num_of_students += 1 
+
+def fullname(self):
+  return '{} {}'.format(self.first, self.last)
+
+def apply_loweredloan(self):
+  self.loan = int(self.loan * lowered_loan)
+
+stud_1 = Student('Anabel', 'Alexander', 50000)
+stud_2 = Student('testing this', 'user', 509900)
